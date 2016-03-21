7725870eaa27f75f3d7618e9eddf64416cd2df88
Kyle Robinson Young <kyle@dontkry.com>
2016-03-20 19:49:46 -0700

    Commenting out renderer md

--- app/lib/mdtohtml.js
+++ app/lib/mdtohtml.js
@@ -1,11 +1,20 @@
 var yfm = require('yfm')
 var marked = require('marked')
+var renderer = new marked.Renderer()
+
+// renderer.heading = function (text, level) {
+//   var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-')
+//   return `<h${level}>
+//     <a name="${escapedText}" href="#${escapedText}">${text}</a>
+//   </h${level}>`
+// }
 
 module.exports = function mdtohtml (md) {
   var src = yfm(md.toString())
   if (src.context.ignore || src.context.ignored) {
     return
   }
+  src.context.renderer = renderer
   src.html = marked(src.content, src.context)
   return src
 }
