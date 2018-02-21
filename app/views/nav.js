8b3eda494e5c461a5f89e5c157c5ece79fc0d188
Kyle Robinson Young <kyle@dontkry.com>
2018-02-21 07:14:33 -0800

    Add patreon

--- app/views/nav.js
+++ app/views/nav.js
@@ -5,7 +5,9 @@ module.exports = function nav () {
     <li><a href="/">BLOG</a></li>
     <li><a href="https://youtube.com/kylerobinsonyoung" target="_blank">VIDEOS</a></li>
     <li><a href="/about.html">ABOUT</a></li>
+    <li>---</li>
     <li><a href="https://twitter.com/shamakry" target="_blank">@SHAMAKRY</a></li>
     <li><a href="https://github.com/shama" target="_blank">GITHUB</a></li>
+    <li><a href="https://www.patreon.com/bePatron?u=2381209" data-patreon-widget-type="become-patron-button">Patreon</a></li>
   </ul>`
 }
